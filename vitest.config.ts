b2ea912a7ae60dd182f129dc23b46bbec2710f7f
anubclao <anubclao@users.noreply.github.com>
2026-07-22 23:03:48 -0500

    chore: exclude TEMPLATE test from default vitest run

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'node:path';
 export default defineConfig({
   test: {
     include: ['tests/**/*.spec.ts'],
+    exclude: ['node_modules/**', 'tests/verifiers/TEMPLATE_*.spec.ts'],
     testTimeout: 30_000,
     pool: 'forks',
     poolOptions: { forks: { singleFork: true } },
